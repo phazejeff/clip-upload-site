f83e22e5bcfc731038bec37fed5de62564bcf724
phazejeff <85849384+phazejeff@users.noreply.github.com>
2024-09-29 20:18:00 -0700

    run only runs on main debug

--- app.py
+++ app.py
@@ -49,4 +49,5 @@ def clip(filename: str):
     f.close()
     return render_template("clip.html", title=title, filename=filename)
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
